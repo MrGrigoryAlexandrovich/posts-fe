3b85e456e7752ad82b41550cf193fd729a9b4c14
Ahmed Cvrcak <ahmed@karriera.de>
2025-11-06 14:44:52 +0100

    Revert coe final CI/CD test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PostList from "./components/PostList";
 const Home = () => (
   <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <main className="p-8 max-w-2xl mx-auto space-y-8">
-      <h1 className="text-3xl font-bold text-center text-green-600">
+      <h1 className="text-3xl font-bold text-center text-blue-600">
         Posts CRUD
       </h1>
       <p className="text-2xl leading-1">Create post</p>
